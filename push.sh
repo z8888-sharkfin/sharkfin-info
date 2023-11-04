3af8815325321bfc38a3cb5166ed1da7288b174a
z8888-sharkfin <140867700+z8888-sharkfin@users.noreply.github.com>
2023-11-04 15:19:38 +0700

    add push.sh

---
+++ push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+USER=z8888-sharkfin
+GKEY=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
+
+log() {
+    echo $* >&2
+}
+
+usage() {
+    log "Usage:\n"
+    log "\t./$(basename $0) <comment string>"
+    echo ""
+}
+
+main() {
+    comment="$(shift 0; echo "$*")"
+
+	git config --local user.name $USER
+	git add --all
+	git commit -a -m "$comment" --author $USER
+	git push origin main
+}
+
+main $@
